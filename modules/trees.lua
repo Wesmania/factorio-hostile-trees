4070ef24d1c10455f9f95c7681cae38072cb90ae
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2023-11-28 20:16:18 +0100

    Adjust event probabilities slightly
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- modules/trees.lua
+++ modules/trees.lua
@@ -67,17 +67,17 @@ local events = {
 			end
 		end },
 		{ 0.75, {
-			sum = 1.0,
+			sum = 100,
 			e = {
-				{ 0.07, function(a) tree_events.spit_trees_towards_buildings(a.s, a.t, a.b) end },
-				{ 0.18, function(a) tree_events.spread_trees_towards_buildings(a.s, a.t, a.b) end },
-				{ 0.15, function(a) tree_events.set_tree_on_fire(a.s, a.t) end },
-				{ 0.10, function(a) tree_events.small_tree_explosion(a.s, a.t) end },
-				{ 0.05, function(a) tree_events.turn_tree_into_ent(a.s, a.t) end },
-				{ 0.05, function(a) tree_events.spawn_biters(a.s, a.t.position, math.random(3, 5)) end},
-				{ 0.25, function(a) tree_events.spitter_projectile(a.s, a.t.position, a.b.position) end },
-				{ 0.10, function(a) tree_events.fire_stream(a.s, a.t.position, a.b.position) end },
-				{ 0.05, function(a)
+				{ 18, function(a) tree_events.spread_trees_towards_buildings(a.s, a.t, a.b) end },
+				{ 7, function(a) tree_events.spit_trees_towards_buildings(a.s, a.t, a.b) end },
+				{ 10, function(a) tree_events.set_tree_on_fire(a.s, a.t) end },
+				{ 10, function(a) tree_events.small_tree_explosion(a.s, a.t) end },
+				{ 10, function(a) tree_events.spawn_biters(a.s, a.t.position, math.random(3, 5)) end},
+				{ 10, function(a) tree_events.turn_tree_into_ent(a.s, a.t) end },
+				{ 10, function(a) tree_events.fire_stream(a.s, a.t.position, a.b.position) end },
+				{ 20, function(a) tree_events.spitter_projectile(a.s, a.t.position, a.b.position) end },
+				{ 5, function(a)
 					local projectile_kinds = {
 						{ "spitter_projectile" },
 						{ "fire_stream" },
